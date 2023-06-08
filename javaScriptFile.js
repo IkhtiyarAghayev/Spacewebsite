939282a32fda9b93faf0c4728059957ed1e310f3
IkhtiyarAghayev <112749695+IkhtiyarAghayev@users.noreply.github.com>
2023-06-08 12:16:50 +0400

    Update javaScriptFile.js

--- javaScriptFile.js
+++ javaScriptFile.js
@@ -1,12 +1,38 @@
-let navbtns = document.querySelectorAll(".btn");
-let anchorTag = document.querySelectorAll(".anchorTag");
-navbtns.forEach(btn => {
-    btn.addEventListener("click", function (e) {
-        console.log(e);
-        navbtns.forEach(btn => {
-            btn.classList.remove("activebtn");
+let planetBtn = document.querySelectorAll(".planet-btn");
+let absoluteFlexContainer = document.querySelectorAll('.absoluteFlexContainer');
+let fcontainer = document.querySelector(".fcontainer");
+planetBtn.forEach(btn => {
+    // console.log(btn.innerText);
+    btn.addEventListener("click", () => {
+        planetBtn.forEach(btn => {
+            btn.classList.remove("active-p-btn");
         });
-        btn.classList.add("activebtn");
+        btn.classList.add("active-p-btn");
+
+        if (btn.innerText == "Moon") {
+            absoluteFlexContainer.forEach(afc => {
+                afc.classList.remove("active-planet");
+            });
+            document.getElementById('Moon').classList.add("active-planet");
+        }
+        if (btn.innerText == "Mars") {
+            absoluteFlexContainer.forEach(afc => {
+                afc.classList.remove("active-planet");
+            });
+            document.getElementById('Mars').classList.add("active-planet");
+        }
+        if (btn.innerText == "Europa") {
+            absoluteFlexContainer.forEach(afc => {
+                afc.classList.remove("active-planet");
+            });
+            document.getElementById('Europa').classList.add("active-planet");
+        }
+        if (btn.innerText == "Titan") {
+            absoluteFlexContainer.forEach(afc => {
+                afc.classList.remove("active-planet");
+            });
+            document.getElementById('Titan').classList.add("active-planet");
+        }
+
     });
 });
-
